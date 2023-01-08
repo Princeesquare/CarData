62eb3db7630b0aef6fc59452baaeab1bc03a336f
Princeesquare <104214893+Princeesquare@users.noreply.github.com>
2023-01-09 01:23:21 +0200

    Add files via upload

--- Listview/Properties/AssemblyInfo.cs
+++ Listview/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Listview")]
+[assembly: AssemblyTitle("CarData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Listview")]
+[assembly: AssemblyProduct("CarData")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
